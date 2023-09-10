157a81587e10ced229bb92868a269fce01a0fb09
lelouvincx <dinhminhchinh3357@gmail.com>
2023-09-10 09:59:56 +0700

    Add lazyvim configs

---
+++ lazyvim/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Disable builtin plugins for faster startup
+vim.g.loaded_python3_provider = 1
+vim.g.loaded_python_provider = 1
+vim.g.loaded_node_provider = 1
+vim.g.loaded_ruby_provider = 1
+vim.g.loaded_perl_provider = 1
