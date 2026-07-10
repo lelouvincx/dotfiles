635938ce458de6c9d402addb6de2edf5b565bb78
lelouvincx <dinhminhchinh3357@gmail.com>
2026-07-10 11:31:31 +0700

    feat: herdr agent switching, agent-browser, and shell quality-of-life
    - herdr: add agent switching keybindings (prefix+K/J), set agent_panel_sort to spaces, disable toast/sound notifications
    - mise: add npm:agent-browser tool
    - zsh: add quit/q aliases for exit, remove empty GITHUB_TOKEN env var

--- zshrc/.zsh/env.zsh
+++ zshrc/.zsh/env.zsh
@@ -4,9 +4,6 @@
 # Default editor
 export EDITOR=nvim
 
-# GitHub token (consider using a secure method to store this)
-export GITHUB_TOKEN=
-
 # Tool selection variables
 export LS_EXECUTABLE=lsd # either lsd or exa
 export LANGUAGE_EXECUTABLE=mise # either asdf or mise
